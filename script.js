25e49a5e60658413f960b54eacfb2f38fcb9348d
technologygeeks <geeksc6@gmail.com>
2024-04-11 15:38:53 +0530

    js file to interact the site

---
+++ script.js
@@ -0,0 +1,30 @@
+let darkmode=document.querySelector("#dark-mode");
+darkmode.onclick=()=>{
+    if(darkmode.classList.contains('bx-moon')){
+        darkmode.classList.replace('bx-moon','bx-sun');
+        document.body.classList.add('color');
+    }else{
+        darkmode.classList.replace('bx-sun','bx-moon');
+        document.body.classList.remove('color');
+    }
+}
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector(".navlist");
+menu.onclick =() =>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+window.onscroll =()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('open');
+}
+
+// const sr=Scrollreveal({
+//     distance:'70px',
+//     duration:2700,
+//     reset:true
+// });
+// sr.reveal('.hero-text',{delay:200,origin:'bottom'});
+// sr.reveal('.hero-img',{delay:350,origin:'bottom'});
+// sr.reveal('.down-arrow',{delay:450,origin:'bottom'});
